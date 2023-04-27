75de349e257e347abf56817d14bf7abbc12e21fc
mAdutskevich <m.adutskevich@gmail.com>
2023-04-27 21:01:47 +0200

    cleanup

--- loadIncrement.js
+++
@@ -1,8 +0,0 @@
-import factory from './increment.js';
-
-factory().then((instance) => {
-  console.log('instance', instance);
-  instance._sayHi(); // direct calling works
-  instance.ccall("sayHi"); // using ccall etc. also work
-  console.log(instance._daysInWeek()); // values can be returned, etc.
-});
--- loadMath.js
+++
@@ -1,5 +0,0 @@
-import loadMath from './math.js';
-const wasm_module = await loadMath();
-
-console.log(wasm_module._add(2,3));
-console.log(wasm_module._substract(8,2));
